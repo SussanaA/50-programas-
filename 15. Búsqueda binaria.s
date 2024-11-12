b56f856fddb261172179e188d6d52627fe8c5b30
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 15:33:49 -0800

    Update and rename 15. .s to 15. Búsqueda binaria.s

---
+++ 15. Búsqueda binaria.s
@@ -0,0 +1,49 @@
+// Instituto Tecnológico de Tijuana
+// Departamento de Sistemas y Computación
+// Materia: Lenguajes de Interfaz
+
+// Nombre del alumno:** Rodríguez Morales Susana Abigail
+// No. control:** 22210346
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+// Código en C#
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Código en ensamblador ARM64
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar el programa
+
+// Ensamblar el código
+// as -o bin.o bin.s
+
+// Vincular el archivo objeto
+// ld -o bin bin.o
+
+// Ejecutar el programa
+//  ./bin
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar en gdb
+
+// gdb -q bin
+
+// start
+
+// step
+
+// q
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Enlace de asciinema
+
+
+
+
+
+-
