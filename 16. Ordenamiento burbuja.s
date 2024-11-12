42821a56be535e793a79dcccdcf31ea1e5dac90f
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 15:36:53 -0800

    Update 16. Ordenamiento burbuja.s

--- 16. Ordenamiento burbuja.s
+++ 16. Ordenamiento burbuja.s
@@ -2,9 +2,48 @@
 // Departamento de Sistemas y Computación
 // Materia: Lenguajes de Interfaz
 
-// Nombre del alumno:** Rodríguez Morales Susana Abigail  
-// No. control:** 22210346  
+// Nombre del alumno:** Rodríguez Morales Susana Abigail
+// No. control:** 22210346
 
 //-------------------------------------------------------------------------------------------------------------------------------------
 
 // Código en C#
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Código en ensamblador ARM64
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar el programa
+
+// Ensamblar el código
+// as -o burb.o burb.s
+
+// Vincular el archivo objeto
+// ld -o burb burb.o
+
+// Ejecutar el programa
+//  ./burb
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar en gdb
+
+// gdb -q burb
+
+// start
+
+// step
+
+// q
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Enlace de asciinema
+
+
+
+
+
+-
