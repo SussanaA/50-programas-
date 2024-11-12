beebc89417c4302bd6796dee637f59cd6d36c369
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 15:40:51 -0800

    Update and rename 20. .s to 20. Multiplicación de matrices.s

---
+++ 20. Multiplicación de matrices.s
@@ -0,0 +1,49 @@
+// Instituto Tecnológico de Tijuana
+// Departamento de Sistemas y Computación
+// Materia: Lenguajes de Interfaz
+
+// Nombre del alumno:** Rodríguez Morales Susana Abigail
+// No. control:** 22210346
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+// Código en C#
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Código en ensamblador ARM64
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar el programa
+
+// Ensamblar el código
+// as -o multmatr.o multmatr.s
+
+// Vincular el archivo objeto
+// ld -o multmatr multmatr.o
+
+// Ejecutar el programa
+//  ./multmatr
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar en gdb
+
+// gdb -q multmatr
+
+// start
+
+// step
+
+// q
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Enlace de asciinema
+
+
+
+
+
+-
