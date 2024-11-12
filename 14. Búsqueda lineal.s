14e76ca40bb746f8704867359a5e6fbc275adb40
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 01:11:30 -0800

    Update and rename 14. .s to 14. Búsqueda lineal.s

---
+++ 14. Búsqueda lineal.s
@@ -0,0 +1,49 @@
+// Instituto Tecnológico de Tijuana
+// Departamento de Sistemas y Computación
+// Materia: Lenguajes de Interfaz
+
+// Nombre del alumno:** Rodríguez Morales Susana Abigail
+// No. control:** 22210346
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+// Código en C#
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Código en ensamblador ARM64
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar el programa
+
+// Ensamblar el código
+// as -o busq.o busq.s
+
+// Vincular el archivo objeto
+// ld -o busq busq.o
+
+// Ejecutar el programa
+//  ./busq
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar en gdb
+
+// gdb -q busq
+
+// start
+
+// step
+
+// q
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Enlace de asciinema
+
+
+
+
+
+-
