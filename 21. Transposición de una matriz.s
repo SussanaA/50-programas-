26e2c988d561da394d3f816f5dfebf44e3274ff5
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 15:41:46 -0800

    Update and rename 21. .s to 21. Transposición de una matriz.s

---
+++ 21. Transposición de una matriz.s
@@ -0,0 +1,49 @@
+// Instituto Tecnológico de Tijuana
+// Departamento de Sistemas y Computación
+// Materia: Lenguajes de Interfaz
+
+// Nombre del alumno:** Rodríguez Morales Susana Abigail
+// No. control:** 22210346
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+// Código en C#
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Código en ensamblador ARM64
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar el programa
+
+// Ensamblar el código
+// as -o tranm.o tranm.s
+
+// Vincular el archivo objeto
+// ld -o tranm tranm.o
+
+// Ejecutar el programa
+//  ./tranm
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar en gdb
+
+// gdb -q tranm
+
+// start
+
+// step
+
+// q
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Enlace de asciinema
+
+
+
+
+
+-
