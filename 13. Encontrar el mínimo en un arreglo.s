59f65e957679715ea4900c8b5a05d3753ae42c7a
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 15:58:20 -0800

    Update 13. Encontrar el mínimo en un arreglo.s

--- 13. Encontrar el mínimo en un arreglo.s
+++ 13. Encontrar el mínimo en un arreglo.s
@@ -9,6 +9,39 @@
 
 // Código en C#
 
+// using System;
+
+// namespace BusquedaNumeroMinimo
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             // Definir un arreglo de ejemplo
+//             int[] numeros = { 23, 5, 89, 15, 42, 3, 77, 12 };
+
+//             // Inicializar el valor mínimo como el primer elemento del arreglo
+//             int numeroMinimo = numeros[0];
+
+//             // Recorrer el arreglo para encontrar el número mínimo
+//             for (int i = 1; i < numeros.Length; i++)
+//             {
+//                 if (numeros[i] < numeroMinimo)
+//                 {
+//                     numeroMinimo = numeros[i];
+//                 }
+//             }
+
+//             // Mostrar el número mínimo encontrado
+//             Console.WriteLine("El número mínimo en el arreglo es: " + numeroMinimo);
+
+//             // Esperar a que el usuario presione una tecla para salir
+//             Console.WriteLine("Presiona cualquier tecla para salir...");
+//             Console.ReadKey();
+//         }
+//     }
+// }
+
 
 //-------------------------------------------------------------------------------------------------------------------------------------
 // Código en ensamblador ARM64
