5a428e064a2a42e8eb150484b5f6172b5ee1dbe2
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 00:19:02 -0800

    Update and rename 09. .s to 09. Verificar si un número es primo.s

---
+++ 09. Verificar si un número es primo.s
@@ -0,0 +1,34 @@
+// Instituto Tecnológico de Tijuana
+// Departamento de Sistemas y Computación
+// Materia: Lenguajes de Interfaz
+
+// Nombre del alumno:** Rodríguez Morales Susana Abigail  
+// No. control:** 22210346  
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+// Código de referencia en C#
+
+
+//-----------------------------------------------------------------------------------------------------------------
+
+// Código en ARM64 Assembly
+
+
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar el programa
+
+// Ensamblar el código
+// as -o primo.o primo.cs
+
+// Vincular el archivo objeto
+// ld -o primo primo.o
+
+// Ejecutar el programa
+//  ./primo
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// Enlace de asciinema
