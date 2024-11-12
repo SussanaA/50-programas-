019d664dcf4e4236a0a23d4f64e796422514860b
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 15:43:20 -0800

    Update and rename 22. .s to 22. Concersión de ASCII a entero.s

---
+++ 22. Concersión de ASCII a entero.s
@@ -0,0 +1,49 @@
+// Instituto Tecnológico de Tijuana
+// Departamento de Sistemas y Computación
+// Materia: Lenguajes de Interfaz
+
+// Nombre del alumno:** Rodríguez Morales Susana Abigail
+// No. control:** 22210346
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+// Código en C#
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Código en ensamblador ARM64
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar el programa
+
+// Ensamblar el código
+// as -o asccint.o asccint.s
+
+// Vincular el archivo objeto
+// ld -o asccint asccint.o
+
+// Ejecutar el programa
+//  ./asccint
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar en gdb
+
+// gdb -q asccint
+
+// start
+
+// step
+
+// q
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Enlace de asciinema
+
+
+
+
+
+-
