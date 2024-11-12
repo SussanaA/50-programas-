d12d34f28045047208f514ca71ebd224185dcb50
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 15:37:41 -0800

    Update and rename 17. .s to 17. Ordenamiento por selección.s

---
+++ 17. Ordenamiento por selección.s
@@ -0,0 +1,49 @@
+// Instituto Tecnológico de Tijuana
+// Departamento de Sistemas y Computación
+// Materia: Lenguajes de Interfaz
+
+// Nombre del alumno:** Rodríguez Morales Susana Abigail
+// No. control:** 22210346
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+// Código en C#
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Código en ensamblador ARM64
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar el programa
+
+// Ensamblar el código
+// as -o sel.o sel.s
+
+// Vincular el archivo objeto
+// ld -o sel sel.o
+
+// Ejecutar el programa
+//  ./sel
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar en gdb
+
+// gdb -q sel
+
+// start
+
+// step
+
+// q
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Enlace de asciinema
+
+
+
+
+
+-
