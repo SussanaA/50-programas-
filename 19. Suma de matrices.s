8fcaffea245f737da70fe8af6401b8d94bf602c4
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 15:39:26 -0800

    Update and rename 19. .s to 19. Suma de matrices.s

---
+++ 19. Suma de matrices.s
@@ -0,0 +1,49 @@
+// Instituto Tecnológico de Tijuana
+// Departamento de Sistemas y Computación
+// Materia: Lenguajes de Interfaz
+
+// Nombre del alumno:** Rodríguez Morales Susana Abigail
+// No. control:** 22210346
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+// Código en C#
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Código en ensamblador ARM64
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar el programa
+
+// Ensamblar el código
+// as -o matr.o matr.s
+
+// Vincular el archivo objeto
+// ld -o matr matr.o
+
+// Ejecutar el programa
+//  ./matr
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar en gdb
+
+// gdb -q matr
+
+// start
+
+// step
+
+// q
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Enlace de asciinema
+
+
+
+
+
+-
