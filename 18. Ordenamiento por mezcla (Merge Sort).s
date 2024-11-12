36a636eddeec13a255f7c2c0749682d8baa67c5b
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 15:38:41 -0800

    Update and rename 18. .s to 18. Ordenamiento por mezcla (Merge Sort).s

---
+++ 18. Ordenamiento por mezcla (Merge Sort).s
@@ -0,0 +1,49 @@
+// Instituto Tecnológico de Tijuana
+// Departamento de Sistemas y Computación
+// Materia: Lenguajes de Interfaz
+
+// Nombre del alumno:** Rodríguez Morales Susana Abigail
+// No. control:** 22210346
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+// Código en C#
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Código en ensamblador ARM64
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar el programa
+
+// Ensamblar el código
+// as -o merg.o merg.s
+
+// Vincular el archivo objeto
+// ld -o merg merg.o
+
+// Ejecutar el programa
+//  ./bin
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar en gdb
+
+// gdb -q merg
+
+// start
+
+// step
+
+// q
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Enlace de asciinema
+
+
+
+
+
+-
