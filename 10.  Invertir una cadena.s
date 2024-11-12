a6d20a7e67d66416a4a54f3f0c4885076da84558
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 00:21:50 -0800

    Update and rename 10.  nvertir una cadena.s to 10.  Invertir una cadena.s

---
+++ 10.  Invertir una cadena.s
@@ -0,0 +1,41 @@
+// Instituto Tecnológico de Tijuana
+// Departamento de Sistemas y Computación
+// Materia: Lenguajes de Interfaz
+
+// Nombre del alumno:** Rodríguez Morales Susana Abigail  
+// No. control:** 22210346  
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+/// Código de referencia en C#
+
+
+//-----------------------------------------------------------------------------------------------------------------
+
+// Código en ARM64 Assembly
+
+
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar el programa
+
+// Ensamblar el código
+// as -o inv.o inv.s
+
+// Vincular el archivo objeto
+// ld -o inv inv.o
+
+// Ejecutar el programa
+//  ./inv
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// Enlace de asciinema
+
+
+
+
+
+
+-
