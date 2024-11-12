c2ca45cb3c9334da5b50c3166d09b460532cf13f
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 00:54:40 -0800

    Update and rename 11. .s to 11. Verificar si una cadena es palíndromo.s

---
+++ 11. Verificar si una cadena es palíndromo.s
@@ -0,0 +1,39 @@
+// Instituto Tecnológico de Tijuana
+// Departamento de Sistemas y Computación
+// Materia: Lenguajes de Interfaz
+
+// Nombre del alumno:** Rodríguez Morales Susana Abigail
+// No. control:** 22210346
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+// Código de referencia en C#
+
+//-----------------------------------------------------------------------------------------------------------------
+
+// Código en ARM64 Assembly
+
+
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar el programa
+
+// Ensamblar el código
+// as -o palind.o palind.s
+
+// Vincular el archivo objeto
+// ld -o palind palind.o
+
+// Ejecutar el programa
+//  ./palind
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// Enlace de asciinema
+
+
+
+
+
+-
