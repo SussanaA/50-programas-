9691170fabb4a1ea1678a33351098c565f570851
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 01:10:23 -0800

    Update and rename 13. .s to 13. Encontrar el mínimo en un arreglo.s

---
+++ 13. Encontrar el mínimo en un arreglo.s
@@ -0,0 +1,49 @@
+// Instituto Tecnológico de Tijuana
+// Departamento de Sistemas y Computación
+// Materia: Lenguajes de Interfaz
+
+// Nombre del alumno:** Rodríguez Morales Susana Abigail
+// No. control:** 22210346
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+// Código en C#
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Código en ensamblador ARM64
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar el programa
+
+// Ensamblar el código
+// as -o min.o min.s
+
+// Vincular el archivo objeto
+// ld -o min min.o
+
+// Ejecutar el programa
+//  ./min
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Comandos para ejecutar en gdb
+
+// gdb -q min
+
+// start
+
+// step
+
+// q
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+// Enlace de asciinema
+
+
+
+
+
+-
