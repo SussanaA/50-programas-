49bb7377e21c8276f573de0d77359b8c85a4a86c
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-12 00:08:18 -0800

    Update 9. .s

--- 9. .s
+++ 9. .s
@@ -1,1 +1,10 @@
-//
+// Instituto Tecnológico de Tijuana
+// Departamento de Sistemas y Computación
+// Materia: Lenguajes de Interfaz
+
+// Nombre del alumno:** Rodríguez Morales Susana Abigail  
+// No. control:** 22210346  
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+// Código en C#
