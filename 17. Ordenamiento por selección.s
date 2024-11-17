09457877913b3e69d5a07a45a70218bc92e0edbb
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-16 21:42:12 -0800

    Update 17. Ordenamiento por selección.s

--- 17. Ordenamiento por selección.s
+++ 17. Ordenamiento por selección.s
@@ -6,14 +6,89 @@
 // No. control:** 22210346
 
 //-------------------------------------------------------------------------------------------------------------------------------------
-
 // Código en C#
 
+// using System;
 
-//-------------------------------------------------------------------------------------------------------------------------------------
+// namespace OrdenamientoPorSeleccion
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             // Solicitar al usuario la cantidad de elementos
+//             Console.Write("Ingrese la cantidad de elementos del arreglo: ");
+//             int n = Convert.ToInt32(Console.ReadLine());
+            
+//             // Crear el arreglo
+//             int[] arreglo = new int[n];
+            
+//             // Solicitar al usuario los elementos del arreglo
+//             Console.WriteLine("Ingrese los elementos del arreglo:");
+//             for (int i = 0; i < n; i++)
+//             {
+//                 Console.Write($"Elemento {i + 1}: ");
+//                 arreglo[i] = Convert.ToInt32(Console.ReadLine());
+//             }
+
+//             // Mostrar el arreglo original
+//             Console.WriteLine("\nArreglo original:");
+//             MostrarArreglo(arreglo);
+
+//             // Ordenar el arreglo utilizando el algoritmo de selección
+//             OrdenamientoPorSeleccion(arreglo);
+
+//             // Mostrar el arreglo ordenado
+//             Console.WriteLine("\nArreglo ordenado:");
+//             MostrarArreglo(arreglo);
+
+//             // Esperar a que el usuario presione una tecla antes de salir
+//             Console.WriteLine("\nPresione cualquier tecla para salir...");
+//             Console.ReadKey();
+//         }
+
+//         // Método para implementar el ordenamiento por selección
+//         static void OrdenamientoPorSeleccion(int[] arreglo)
+//         {
+//             int n = arreglo.Length;
+//             for (int i = 0; i < n - 1; i++)
+//             {
+//                 // Encontrar el índice del menor elemento en el arreglo desde i hasta el final
+//                 int indiceMenor = i;
+//                 for (int j = i + 1; j < n; j++)
+//                 {
+//                     if (arreglo[j] < arreglo[indiceMenor])
+//                     {
+//                         indiceMenor = j;
+//                     }
+//                 }
+
+//                 // Intercambiar el elemento actual con el menor encontrado
+//                 if (indiceMenor != i)
+//                 {
+//                     int temp = arreglo[i];
+//                     arreglo[i] = arreglo[indiceMenor];
+//                     arreglo[indiceMenor] = temp;
+//                 }
+//             }
+//         }
+
+//         // Método para mostrar el contenido de un arreglo
+//         static void MostrarArreglo(int[] arreglo)
+//         {
+//             foreach (int elemento in arreglo)
+//             {
+//                 Console.Write(elemento + " ");
+//             }
+//             Console.WriteLine();
+//         }
+//     }
+// }
+
+//-----------------------------------------------------------------------------------------------------------------
 // Código en ensamblador ARM64
 
-//--------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------
 
 // Comandos para ejecutar el programa
 
@@ -26,7 +101,7 @@
 // Ejecutar el programa
 //  ./sel
 
-//--------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------
 
 // Comandos para ejecutar en gdb
 
@@ -39,7 +114,7 @@
 // q
 
 
-//-------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------
 // Enlace de asciinema
 
 
