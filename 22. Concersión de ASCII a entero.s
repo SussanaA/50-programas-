d8bb957b16b2bbf8b1050a697cc51d66cf79781e
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-18 12:20:35 -0800

    Update 22. Concersión de ASCII a entero.s

--- 22. Concersión de ASCII a entero.s
+++ 22. Concersión de ASCII a entero.s
@@ -1,3 +1,7 @@
+En esta actividad se estará reutilizando la actividad 2, ya que para realizar la suma
+se necesito de realizar una conversion de entero a ASCII.
+
+//-------------------------------------------------------------------------------------------------------------------------------------
 // Instituto Tecnológico de Tijuana
 // Departamento de Sistemas y Computación
 // Materia: Lenguajes de Interfaz
@@ -6,12 +10,29 @@
 // No. control:** 22210346
 
 //-------------------------------------------------------------------------------------------------------------------------------------
-
 // Código en C#
+//  Console.WriteLine("Programa de Suma de Dos Números");
 
+            // Solicitar el primer número
+//            Console.Write("Introduce el primer número: ");
+//            string entrada1 = Console.ReadLine();
+//            int numero1 = int.Parse(entrada1);
+
+            // Solicitar el segundo número
+//            Console.Write("Introduce el segundo número: ");
+//            string entrada2 = Console.ReadLine();
+//            int numero2 = int.Parse(entrada2);
+
+            // Realizar la suma
+//            int suma = numero1 + numero2;
+
+            // Mostrar el resultado
+//            Console.WriteLine("La suma de {0} y {1} es: {2}", numero1, numero2, suma);
 
 //-------------------------------------------------------------------------------------------------------------------------------------
 // Código en ensamblador ARM64
+
+
 
 //--------------------------------------------------------------------------------------------------------------
 
@@ -46,4 +67,5 @@
 
 
 
--
+//----------------------------------------------------------------------------------------------------------------------
+
