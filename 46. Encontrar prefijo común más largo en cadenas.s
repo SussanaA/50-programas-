b1377a2a216ce512723d896753c21e675ea0a6d0
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-19 14:07:30 -0800

    Update 46. Encontrar prefijo común más largo en cadenas.s

--- 46. Encontrar prefijo común más largo en cadenas.s
+++ 46. Encontrar prefijo común más largo en cadenas.s
@@ -9,6 +9,73 @@
 //----------------------------------------------------------------------------------------------------------------------
 // Código en C#
 
+// using System;
+
+// namespace PrefijoComun
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             // Solicitar al usuario que ingrese las cadenas
+//             Console.Write("Ingrese el número de cadenas: ");
+//             int n = int.Parse(Console.ReadLine());
+
+//             string[] cadenas = new string[n];
+
+//             // Leer las cadenas
+//             for (int i = 0; i < n; i++)
+//             {
+//                 Console.Write($"Ingrese la cadena {i + 1}: ");
+//                 cadenas[i] = Console.ReadLine();
+//             }
+
+//             // Llamar al método para encontrar el prefijo común más largo
+//             string prefijoComun = EncontrarPrefijoComunMasLargo(cadenas);
+
+//             // Mostrar el resultado
+//             if (string.IsNullOrEmpty(prefijoComun))
+//             {
+//                 Console.WriteLine("No hay un prefijo común.");
+//             }
+//             else
+//             {
+//                 Console.WriteLine("El prefijo común más largo es: " + prefijoComun);
+//             }
+
+//             // Esperar a que el usuario presione una tecla para salir
+//             Console.WriteLine("Presione cualquier tecla para salir...");
+//             Console.ReadKey();
+//         }
+
+//         // Método que encuentra el prefijo común más largo
+//         static string EncontrarPrefijoComunMasLargo(string[] cadenas)
+//         {
+//             if (cadenas == null || cadenas.Length == 0)
+//                 return "";
+
+//             // Suponemos que el prefijo más largo es la primera cadena
+//             string prefijoComun = cadenas[0];
+
+//             // Comparamos el prefijo con cada cadena
+//             foreach (string cadena in cadenas)
+//             {
+//                 // Compara el prefijo con la cadena actual
+//                 while (cadena.IndexOf(prefijoComun) != 0)
+//                 {
+//                     // Recorta el último carácter del prefijo hasta encontrar coincidencia
+//                     prefijoComun = prefijoComun.Substring(0, prefijoComun.Length - 1);
+
+//                     // Si no hay prefijo común, retornamos una cadena vacía
+//                     if (prefijoComun == "")
+//                         return "";
+//                 }
+//             }
+
+//             return prefijoComun;
+//         }
+//     }
+// }
 
 
 //----------------------------------------------------------------------------------------------------------------------
