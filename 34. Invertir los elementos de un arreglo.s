831691925eaa48588e3537061d3bd58d40d36ee3
SussanaA <158237443+SussanaA@users.noreply.github.com>
2024-11-19 12:30:13 -0800

    Update 34. Invertir los elementos de un arreglo.s

--- 34. Invertir los elementos de un arreglo.s
+++ 34. Invertir los elementos de un arreglo.s
@@ -9,6 +9,62 @@
 //----------------------------------------------------------------------------------------------------------------------
 // Código en C#
 
+// using System;
+
+// namespace InvertirArreglo
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             // Crear un arreglo de ejemplo
+//             int[] arreglo = { 1, 2, 3, 4, 5 };
+
+//             // Mostrar el arreglo original
+//             Console.WriteLine("Arreglo original:");
+//             MostrarArreglo(arreglo);
+
+//             // Invertir el arreglo
+//             InvertirArreglo(arreglo);
+
+//             // Mostrar el arreglo invertido
+//             Console.WriteLine("Arreglo invertido:");
+//             MostrarArreglo(arreglo);
+
+//             // Esperar una tecla para salir
+//             Console.ReadKey();
+//         }
+
+//         // Método para invertir el arreglo
+//         static void InvertirArreglo(int[] arreglo)
+//         {
+//             int inicio = 0;
+//             int fin = arreglo.Length - 1;
+
+//             while (inicio < fin)
+//             {
+//                 // Intercambiar los elementos
+//                 int temp = arreglo[inicio];
+//                 arreglo[inicio] = arreglo[fin];
+//                 arreglo[fin] = temp;
+
+//                 // Mover los índices
+//                 inicio++;
+//                 fin--;
+//             }
+//         }
+
+//         // Método para mostrar el arreglo en consola
+//         static void MostrarArreglo(int[] arreglo)
+//         {
+//             foreach (int numero in arreglo)
+//             {
+//                 Console.Write(numero + " ");
+//             }
+//             Console.WriteLine(); // Nueva línea después de mostrar el arreglo
+//         }
+//     }
+// }
 
 
 //----------------------------------------------------------------------------------------------------------------------
